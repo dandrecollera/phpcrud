d051b25772fcbfdf79e58b64487556b41b987c7f
dandrecollera <dandrecollera@gmail.com>
2022-04-18 19:03:20 +0800

    modified

--- vonny/main.php
+++ vonny/main.php
@@ -12,22 +12,7 @@
 
 <body class="bg-dark">
     
-<section class="container p-5 col-12 col-md-6" id="loginForm">
-        <div class="card bg-light text-dark shadow ">
-            <div class="card-header">Welcome</div>
-            <div class="card-body">
-                <h1 class="card-title mb-3">Main Menu</h1>
-                <h6 class="card-subtitle mb-3">Select your options.</h6>
-                <hr class="mb-4">
-                <p?>Patient</p>
-                <a href="./addpatient.php"><button type="button" class="btn btn-outline-primary">Add Patient</button></a>
-                <a href="./listpatient.php"><button type="button" class="btn btn-outline-primary">List of Patient</button></a>
-
-            </div>
-            <div class="card-footer">
-            </div>
-        </div>
-    </section>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
